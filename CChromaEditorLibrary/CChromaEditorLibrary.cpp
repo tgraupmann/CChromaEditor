da3edf25e7c0fea145893c9f4637acdf49b54dcc
Tim Graupmann <tgraupmann@gmail.com>
2018-01-09 17:26:39 -0800

    Implemented SaveAs

--- CChromaEditorLibrary/CChromaEditorLibrary.cpp
+++ CChromaEditorLibrary/CChromaEditorLibrary.cpp
@@ -1937,7 +1937,40 @@ void CMainViewDlg::OnBnClickedMenuSaveAs()
 	// stop animation
 	OnBnClickedButtonStop();
 
-	SaveFile();
+	// get path from loaded filename
+	CString szDir;
+	size_t lastSlash = _mPath.find_last_of("/\\");
+	if (lastSlash < 0)
+	{
+		return;
+	}
+	string path = _mPath.substr(0, lastSlash);
+	//LogDebug("ImportTextureAnimation path=%s", path.c_str());
+	szDir += path.c_str();
+
+	const int MAX_CFileDialog_FILE_COUNT = 99;
+	const int FILE_LIST_BUFFER_SIZE = ((MAX_CFileDialog_FILE_COUNT * (MAX_PATH + 1)) + 1);
+
+	CString fileName;
+	wchar_t* p = fileName.GetBuffer(FILE_LIST_BUFFER_SIZE);
+	CFileDialog dlgFile(TRUE);
+	OPENFILENAME& ofn = dlgFile.GetOFN();
+	ofn.lpstrFilter = _TEXT("Animation\0*.chroma\0");
+	ofn.lpstrInitialDir = szDir;
+	ofn.lpstrFile = p;
+	ofn.nMaxFile = FILE_LIST_BUFFER_SIZE;
+
+	if (dlgFile.DoModal() == IDOK)
+	{
+		_mPath = string(CT2CA(fileName));
+		if (_mPath.size() <= 2 ||
+			_mPath.substr(_mPath.find_last_of(".") + 1) != "chroma")
+		{
+			_mPath += ".chroma";
+		}
+		SaveFile();
+	}
+	fileName.ReleaseBuffer();
 }
 
 void CMainViewDlg::OnBnClickedMenuExit()
